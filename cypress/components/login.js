514913f4b3442a6ee5953a4199e1cd3fee9418c8
Taiwo Odetola <taiwo.odetolla@gmail.com>
2022-01-05 00:06:12 +0100

    add login button

--- cypress/components/login.js
+++ cypress/components/login.js
@@ -1,2 +1,12 @@
 export const email = () => cy.xpath("//input[@type='email']")
 export const password = () => cy.xpath("//input[@name='password']")
+
+export const loginButton = () => cy.xpath("//button[contains(text(),'Login')]")
+
+export const performLogin = (email, password) => {
+    email().clear().type(email)
+    password().clear().type(password)
+    loginButton().click()
+} 
+
+
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -13,32 +13,28 @@ describe("Login Feature", ()=>{
 
     context("Invalid Details - Invalid Password", ()=>{
         it("", ()=>{
-            login.email().clear().type('engineering@mainteny.com')
-            login.password().clear().type('MAINTENY@2021')
+            login.performLogin('engineering@mainteny.com','MAINTENY@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Invalid Details - Invalid Email", ()=>{
         it("", ()=>{
-            login.email().clear().type('taiwo@mainteny.com')
-            login.password().clear().type('Mainteny@2021')
+            login.performLogin('taiwo@mainteny.com','Mainteny@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Invalid Details - Invalid Password and Email", ()=>{
         it("", ()=>{
-            login.email().clear().type('taiwo@mainteny.com')
-            login.password().clear().type('MAINTENY@2021')
+            login.performLogin('taiwo@mainteny.com','MAINTENY@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Valid Details", ()=>{
         it("", ()=>{
-            login.email().clear().type('engineering@mainteny.com')
-            login.password().clear().type('Mainteny@2021')
+            login.performLogin('engineering@mainteny.com','Mainteny@2021')
         })
     })
 })
