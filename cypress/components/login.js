7517d37222e8cbadc2c980f6f2067eebbaabcda0
Taiwo Odetola <taiwo.odetolla@gmail.com>
2022-01-04 23:59:17 +0100

    add a component file

---
+++ cypress/components/login.js
@@ -0,0 +1,2 @@
+export const email = () => cy.xpath("//input[@type='email']")
+export const password = () => cy.xpath("//input[@name='password']")
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,6 +1,8 @@
 /// <reference types="Cypress" />
 /// <reference types="cypress-xpath" />
 
+import * as login from '../components/login.js'
+
 
 describe("Login Feature", ()=>{
 
@@ -11,32 +13,32 @@ describe("Login Feature", ()=>{
 
     context("Invalid Details - Invalid Password", ()=>{
         it("", ()=>{
-            cy.xpath("//input[@type='email']").clear().type('engineering@mainteny.com')
-            cy.xpath("//input[@name='password']").clear().type('MAINTENY@2021')
+            login.email().clear().type('engineering@mainteny.com')
+            login.password().clear().type('MAINTENY@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Invalid Details - Invalid Email", ()=>{
         it("", ()=>{
-            cy.xpath("//input[@type='email']").clear().type('taiwo@mainteny.com')
-            cy.xpath("//input[@name='password']").clear().type('Mainteny@2021')
+            login.email().clear().type('taiwo@mainteny.com')
+            login.password().clear().type('Mainteny@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Invalid Details - Invalid Password and Email", ()=>{
         it("", ()=>{
-            cy.xpath("//input[@type='email']").clear().type('taiwo@mainteny.com')
-            cy.xpath("//input[@name='password']").clear().type('MAINTENY@2021')
+            login.email().clear().type('taiwo@mainteny.com')
+            login.password().clear().type('MAINTENY@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Valid Details", ()=>{
         it("", ()=>{
-            cy.xpath("//input[@type='email']").clear().type('engineering@mainteny.com')
-            cy.xpath("//input[@name='password']").clear().type('Mainteny@2021')
+            login.email().clear().type('engineering@mainteny.com')
+            login.password().clear().type('Mainteny@2021')
         })
     })
 })
