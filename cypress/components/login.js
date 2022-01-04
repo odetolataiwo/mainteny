58a1ebf1d18446ea45fef2fdd21ee179caf1a6d0
Taiwo Odetola <taiwo.odetolla@gmail.com>
2022-01-05 00:17:36 +0100

    added necessary validations

--- cypress/components/login.js
+++ cypress/components/login.js
@@ -1,11 +1,11 @@
-export const email = () => cy.xpath("//input[@type='email']")
-export const password = () => cy.xpath("//input[@name='password']")
+export const emailField = () => cy.xpath("//input[@type='email']")
+export const passwordField = () => cy.xpath("//input[@name='password']")
 
 export const loginButton = () => cy.xpath("//button[contains(text(),'Login')]")
 
 export const performLogin = (email, password) => {
-    email().clear().type(email)
-    password().clear().type(password)
+    emailField().clear().type(email)
+    passwordField().clear().type(password)
     loginButton().click()
 } 
 
--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -6,35 +6,38 @@ import * as login from '../components/login.js'
 
 describe("Login Feature", ()=>{
 
-    before("", ()=>{
+    before("Visit the login page", ()=>{
         // visit the site
         cy.visit("/auth/login")
     })
 
     context("Invalid Details - Invalid Password", ()=>{
-        it("", ()=>{
+        it("Should throw an error", ()=>{
             login.performLogin('engineering@mainteny.com','MAINTENY@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Invalid Details - Invalid Email", ()=>{
-        it("", ()=>{
+        it("Should throw an error", ()=>{
             login.performLogin('taiwo@mainteny.com','Mainteny@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Invalid Details - Invalid Password and Email", ()=>{
-        it("", ()=>{
+        it("Should throw an error", ()=>{
             login.performLogin('taiwo@mainteny.com','MAINTENY@2021')
             cy.xpath("//div[@class='error']").should("be.visible")
         })
     })
 
     context("Valid Details", ()=>{
-        it("", ()=>{
+        it("Should login successfully", ()=>{
             login.performLogin('engineering@mainteny.com','Mainteny@2021')
+
+            cy.url().should('include', '/pages/dashboard')
+            cy.xpath("//div[contains(text(),'Hi Martin!')]").should('be.visible')
         })
     })
 })
